bddbdb4daf1f62c0269f06aa95a525d088acb4d7
Marcus Becker <mabecker@ualberta.ca>
2019-03-15 15:05:08 -0600

    random code

--- Scripts/DataClean_v1.R
+++ Scripts/DataClean_v1.R
@@ -34,12 +34,72 @@ df_tel <- metrics_m %>%
   View()
 
 df_year <- metrics_m %>%
-  filter(Method == "CensusFlights") %>%
-  group_by(StartYear) %>%
-  count()
+  filter(Method == "CensusFlights",
+         WMU == "515")
 
 twentyeighteen <- metrics_m %>%
   filter(Method == "CensusFlights",
          StartYear == "2018")
+
+#-------------------------------------------------------------------------------
+
+library(mapproj)
+library(sf)
+library(tidyverse)
+
+
+# Add OSM Moose sightings data
+
+osm_moose <- read_csv("./Data/Raw/OSM_Ungulate_Survey_Data_2013-2018.csv")
+
+osm_moose <- osm_moose %>%
+  # Change NAs to WMU 515
+  mutate(WMU = if_else(is.na(WMU), 515, WMU)) %>%
+  filter(SPECIES == "MOOSE -MOOS") %>%
+  select(-DATE_1) %>%
+  mutate(WMU = as.character(WMU))
+
+osm_moose %>%
+  ggplot(aes(x = LONG, y = LAT, color = WMU)) + 
+  geom_point() +
+  coord_map() +
+  theme_void()
+
+ab_wmu %>%
+  ggplot() +
+  geom_sf()
+
+osm_moose_sf <- osm_moose %>%
+  st_as_sf(coords = c("LONG", "LAT"), crs = 4326, agr = "constant") %>%
+  expandRows("COUNT") %>%
+  st_jitter(0.01)
+
+names(st_geometry(ab_wmu)) = NULL
+
+sightings <-
+ab_wmu %>%
+  separate(WMUNIT_COD, into = c("zeroes", "WMU"), sep = 2, remove = FALSE) %>%
+  select(-zeroes) %>%
+  filter(WMU %in% osm_moose_sf$WMU) %>%
+  leaflet() %>%
+  addTiles() %>%
+  addProviderTiles("Esri.WorldImagery") %>%
+  
+  addPolygons(color = "#282828", weight = 2, smoothFactor = 0.2, opacity = 2,
+              group = "None") %>%
+  addHeatmap(data = osm_moose_sf, radius = 10, blur = 18, minOpacity = 3)
+              
+sightings
+
+
+
+
+
+
+
+
+
+
+
 
 
