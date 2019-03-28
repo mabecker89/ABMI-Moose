0a12eaa199da90c2ecd265c4321ec8fd38ee1125
Marcus Becker <mabecker@ualberta.ca>
2019-03-28 15:42:20 -0600

    minor changes

--- Scripts/DataClean_v1.R
+++ Scripts/DataClean_v1.R
@@ -47,12 +47,33 @@ twentyeighteen <- metrics_m %>%
 
 # Government aerial observation data
 
-gov_df <- metrics_m %>%
-  filter(Method == "CensusFlights" | is.na(Method))
+dens_df <- metrics %>%
+  filter(Response == "Density",
+         Species == "Moose",
+         !is.na(WMU),
+         !WMU == "506;510" & !WMU == "530 North" & !WMU == "530 South") %>%
+  mutate(WMU = as.numeric(str_replace_all(WMU, "530 Full", "530"))) %>%
+  select(WMU, StartYear, MetricNum) %>%
+  rename(Survey_Year = StartYear,
+         Density = MetricNum) %>%
+  full_join(gov_dens, by = c("WMU", "Survey_Year")) %>%
+  # WMUs 258, 511, and 519 have different density estimates. 
+  mutate(Density = if_else(is.na(Density.y), Density.x, Density.y)) %>%
+  select(-c(Density.x, Density.y)) %>%
+  distinct() %>%
+  mutate(WMU = as.character(WMU))
 
+# Plot density over time
 
+dens <- dens_df %>%
+  filter(Density < 1) %>%
+  ggplot(aes(x = Survey_Year, y = Density, colour = WMU)) +
+  geom_line() +
+  geom_point(size = 2.5)
 
+dens1 <- ggplotly(dens)
 
+dens1
 
 
 
